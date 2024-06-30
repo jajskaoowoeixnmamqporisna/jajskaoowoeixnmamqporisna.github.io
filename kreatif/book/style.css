561e416ea120284d13f7d97a30c30bdd83551d52
jajskaoowoeixnmamqporisna <161032846+jajskaoowoeixnmamqporisna@users.noreply.github.com>
2024-06-30 17:03:18 +0700

    Add files via upload

---
+++ kreatif/book/style.css
@@ -0,0 +1,430 @@
+@import url('https://fonts.googleapis.com/css2?family=Asap:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Russo+One&family=Special+Elite&display=swap');
+
+* {
+    margin: 0;
+    padding: 0;
+}
+
+html {
+    scroll-behavior: smooth;
+}
+
+body {
+    display: flex
+    flex-direction: column
+    clear: both;
+    align-content: center;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    background-color: #fff;
+    transition: background-color 1s ease;
+}
+
+[kezttransition1] {
+    opacity: 0;
+    transform: translateY(100%);
+    transition: opacity 1.5s ease, transform 1.5s ease;
+    position: relative;
+    z-index: -1;
+}
+
+  [kezttransition2] {
+    opacity: 0;
+    transform: translateX(10%);
+    transition: opacity 1.5s ease, transform 1.5s ease;
+    z-index: -1;
+}
+
+  [kezttransition3] {
+    opacity: 0;
+    transform: translateX(-10%);
+    transition: opacity 1.5s ease, transform 1.5s ease;
+    z-index: -1;
+}
+
+  [kezttransition4] {
+    opacity: 0;
+    transform: translateY(-100%);
+    transition: opacity 1.5s ease, transform 1.5s ease;
+    position: relative;
+    z-index: -1;
+}
+
+@font-face  {,
+    font-family: "fsProMedium";
+    src: url("https://g90nf.kezt.my.id/upload/file/sfpro-display-medium.otf");
+    font-weight: normal;
+}
+
+@font-face  {
+    font-family: "willow";
+    src: url("https://g90nf.kezt.my.id/upload/file/willow.ttf");
+    font-weight: normal;
+}
+
+@font-face  {
+    font-family: "Brillotus";
+    src: url("https://g90nf.kezt.my.id/upload/file/brillotus.otf");
+    font-weight: normal;
+}
+
+
+
+@media (max-width: 808px) {
+    
+    .section-baca-buku-njr-v2 {
+        
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr {
+        width: 100%;
+        height: 250px;
+        user-select: none;
+        position: relative;
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group1 {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        color: #fff;
+        background: #00000072;
+        backdrop-filter: blur(10px);
+        border-radius: 10px;
+        display: flex;
+        gap: 15px;
+        padding: 10px;
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group1 > a {
+        font-size: 22px;
+        transition: 0.4s;
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group1 > a:active {
+        font-size: 22px;
+        transform: scale(0.9);
+        transition: 0.4s;
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group2 {
+        position: absolute;
+        top: 170px;
+        left: 50%;
+        transform: translateX(-50%);
+        color: #fff;
+        width: 80%;
+        background: #00000065;
+        backdrop-filter: blur(5px);
+        border-radius: 15px;
+        padding: 10px;
+        transition: 0.5s;
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group2:hover {
+        top: 175px;
+        opacity: 0.6;
+        transition: 0.5s;
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group2 > .username {
+        display: flex;
+        align-items: center;
+        font-family: fsProMedium;
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group2 > .username > img {
+        width: 40px;
+        border-radius: 50%;
+        margin-right: 10px;
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group2 > .username > .container-name {
+        
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group2 > .username > .container-name > h1 {
+        font-family: "Arial", sans-serif;
+        text-align: left;
+        font-size: 17px;
+        color: #fff;
+        margin: 0;
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group2 > .username > .container-name > p {
+        font-family: "Arial", sans-serif;
+        text-align: left;
+        font-size: 10px;
+        color: #d3d3d3;
+        margin: 0;
+    }
+    
+    .section-baca-buku-view {
+        display: flex;
+        flex-direction: column;
+        text-align: left;
+        font-family: fsProMedium;
+        padding: 10px;
+        background: linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)) no-repeat;
+        background-size: 100% 20px;
+    }
+    
+    .section-baca-buku-view > .pembatas {
+        background: #999;
+        height: 3px;
+        border-radius: 50px;
+        width: 100%;
+        margin: 10px 0;        
+    }
+    
+    .section-baca-buku-view > .keterangan-buku {
+        padding: 5px;
+    }
+    
+    .section-baca-buku-view > .keterangan-buku > h1 {
+        font-family: "Arial", sans-serif;
+        text-align: left;
+        font-size: 25px;
+        color: #000;
+        margin: 10px;
+    }
+    
+    .section-baca-buku-view > .keterangan-buku > p {
+        font-family: "Arial", sans-serif;
+        text-align: left;
+        font-size: 15px;
+        color: #000;
+        margin: 10px;
+    }
+    
+    .section-baca-buku-view > .keterangan-buku > .keterangan2 {
+        margin: 10px;
+        background: #a7a7a7;
+        padding: 15px;
+        border-radius: 20px;
+    }
+    
+    .section-baca-buku-view > .keterangan-buku > .keterangan2 > p {
+        font-family: fsProMedium;
+        text-align: left;
+        font-size: 20px;
+        color: #0071E3;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        margin: 5px;
+    }
+    
+    .section-baca-buku-view > .keterangan-buku > .keterangan2 > p > .p2 {
+        transform: translateX(80%);
+        font-family: fsProMedium;
+        text-align: left;
+        font-size: 20px;
+        color: #ffffff;
+    }
+    
+    .bagian-baca-cihuy {
+        padding: 5px;
+    }
+    
+    .bagian-baca-cihuy > h1 {
+        font-family: fsProMedium;
+        text-align: left;
+        font-size: 30px;
+        color: #000;
+    }
+    
+    .bagian-baca-cihuy > p {
+        font-family: fsProMedium;
+        text-align: left;
+        font-size: 17px;
+        color: #000;
+    }
+    
+    
+    
+}
+
+@media (min-width: 808px) {
+    
+    .section-baca-buku-njr-v2 {
+        
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr {
+        width: 100%;
+        height: 250px;
+        user-select: none;
+        position: relative;
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group1 {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        color: #fff;
+        background: #00000072;
+        backdrop-filter: blur(10px);
+        border-radius: 10px;
+        display: flex;
+        gap: 15px;
+        padding: 10px;
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group1 > a {
+        font-size: 22px;
+        transition: 0.4s;
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group1 > a:active {
+        font-size: 22px;
+        transform: scale(0.9);
+        transition: 0.4s;
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group2 {
+        position: absolute;
+        top: 170px;
+        left: 50%;
+        transform: translateX(-50%);
+        color: #fff;
+        width: 70%;
+        background: #00000065;
+        backdrop-filter: blur(5px);
+        border-radius: 15px;
+        padding: 10px;
+        transition: 0.5s;
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group2:hover {
+        top: 175px;
+        opacity: 0.6;
+        transition: 0.5s;
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group2 > .username {
+        display: flex;
+        align-items: center;
+        font-family: fsProMedium;
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group2 > .username > img {
+        width: 40px;
+        border-radius: 50%;
+        margin-right: 10px;
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group2 > .username > .container-name {
+        
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group2 > .username > .container-name > h1 {
+        font-family: "Arial", sans-serif;
+        text-align: left;
+        font-size: 17px;
+        color: #fff;
+        margin: 0;
+    }
+    
+    .section-baca-buku-njr-v2 > .profile-baca-buku-njr > .profile-baca-buku-group2 > .username > .container-name > p {
+        font-family: "Arial", sans-serif;
+        text-align: left;
+        font-size: 10px;
+        color: #d3d3d3;
+        margin: 0;
+    }
+    
+    .section-baca-buku-view {
+        display: flex;
+        flex-direction: column;
+        text-align: left;
+        font-family: fsProMedium;
+        padding: 10px;
+        background: linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)) no-repeat;
+        background-size: 100% 20px;
+    }
+    
+    .section-baca-buku-view > .pembatas {
+        background: #999;
+        height: 3px;
+        border-radius: 50px;
+        width: 100%;
+        margin: 10px 0;        
+    }
+    
+    .section-baca-buku-view > .keterangan-buku {
+        padding: 5px;
+    }
+    
+    .section-baca-buku-view > .keterangan-buku > h1 {
+        font-family: "Arial", sans-serif;
+        text-align: left;
+        font-size: 25px;
+        color: #000;
+        margin: 10px;
+    }
+    
+    .section-baca-buku-view > .keterangan-buku > p {
+        font-family: "Arial", sans-serif;
+        text-align: left;
+        font-size: 15px;
+        color: #000;
+        margin: 10px;
+    }
+    
+    .section-baca-buku-view > .keterangan-buku > .keterangan2 {
+        margin: 10px;
+        background: #a7a7a7;
+        padding: 15px;
+        border-radius: 20px;
+    }
+    
+    .section-baca-buku-view > .keterangan-buku > .keterangan2 > p {
+        font-family: fsProMedium;
+        text-align: left;
+        font-size: 20px;
+        color: #0071E3;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        margin: 5px;
+    }
+    
+    .section-baca-buku-view > .keterangan-buku > .keterangan2 > p > .p2 {
+        transform: translateX(80%);
+        font-family: fsProMedium;
+        text-align: left;
+        font-size: 20px;
+        color: #ffffff;
+    }
+    
+    .bagian-baca-cihuy {
+        padding: 5px;
+    }
+    
+    .bagian-baca-cihuy > h1 {
+        font-family: fsProMedium;
+        text-align: left;
+        font-size: 30px;
+        color: #000;
+    }
+    
+    .bagian-baca-cihuy > p {
+        font-family: fsProMedium;
+        text-align: left;
+        font-size: 17px;
+        color: #000;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
